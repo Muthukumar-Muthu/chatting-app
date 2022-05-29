024f9565148fa8e28ebe525d022b5fae5efd9181
Muthukumar-Muthu <m.m.muthu1388@gmail.com>
2022-05-29 22:46:34 +0530

    done with chat

--- src/components/chat-list/ChatList.jsx
+++ src/components/chat-list/ChatList.jsx
@@ -1,27 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./style.css";
 import Chat from "../chat/Chat";
 import getChat from "../../firebase/functions/getChat";
 const ChatList = ({ chatList }) => {
-  useEffect(() => {
-    if (chatList.length > 0) {
-      getChat(chatList[0].withtestuser1);
-    }
-  }, [chatList]);
-  const array = [
-    {
-      chatImg:
-        "https://images.unsplash.com/photo-1495615080073-6b89c9839ce0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3F1YXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-      chatName: "Important",
-      recentMsg: " 123456789101121222222222222222222222222222",
-    },
-  ];
-  const testMap = array.map((chatObj, index) => (
+  const testMap = chatList.map((chatObj, index) => (
     <Chat
       key={index}
-      chatImg={chatObj.chatImg}
-      chatName={chatObj.chatName}
-      recentMsg={chatObj.recentMsg}
+      path={chatObj.withtestuser1}
+      // chatImg={chatObj.chatImg}
+      // chatName={chatObj.chatName}
+      // recentMsg={chatObj.recentMsg}
     />
   ));
 
--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,14 +1,22 @@
 import "./style.css";
 import trimMsg from "../../functions/trimMsg";
-const Chat = ({ chatImg, chatName, recentMsg }) => {
+import { useEffect, useState } from "react";
+import getChat, { getChatDetail } from "../../firebase/functions/getChat";
+const Chat = ({ path }) => {
+  const [chat, setChat] = useState({});
+  useEffect(() => {
+    getChatDetail(path)
+      .then((response) => setChat(response))
+      .catch((err) => console.warn(err));
+  }, []);
   return (
     <div className="chat">
       <div className="chat-image">
-        <img src={chatImg} alt="" />
+        <img src={chat.chatImg} alt="" />
       </div>
       <div className="chat-details">
-        <h2 className="chat-heading">{chatName}</h2>
-        <p className="chat-msg">{trimMsg(recentMsg, 30)}</p>
+        <h2 className="chat-heading">{chat.chatName}</h2>
+        <p className="chat-msg">{trimMsg(chat.recentMsg, 30)}</p>
       </div>
       <div className="time">12:35 pm</div>
     </div>

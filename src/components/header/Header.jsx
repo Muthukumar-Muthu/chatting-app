abf19bb66b1dfd62cf0da85a2f319a676d668448
Muthukumar-Muthu <m.m.muthu1388@gmail.com>
2022-07-13 22:36:04 +0530

    refactored left bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,34 +1,11 @@
 import "./style.css";
-
+import NewChat from "../newChat/NewChat";
 import { getUserPhotoUrl } from "../../firebase/functions/getUserDetailsFromAuth";
-import UserSettings from "../user-settings/UserSettings";
-import UserPhoto from "../../assests/user-photo.jpeg";
-import { useEffect, useState } from "react";
-import { getChatImgUrl } from "../../firebase/functions/getChatImgUrl";
-const Header = ({
-  setShowUser,
-  userDetails,
-  setShowChatDetials,
-  setShowUserComponent,
-}) => {
-  const [userProfile, setUserProfile] = useState("");
-  // useEffect(() => {
-  //   getChatImgUrl(userDetails.userImg).then(setUserProfile);
-  // }, [userDetails]);
+
+const Header = () => {
   return (
     <div className="header">
-      <div
-        onClick={() => {
-          setShowUserComponent((p) => !p);
-        }}
-        className="profile-picture"
-      >
-        <img src={UserPhoto} alt="" />
-      </div>
-      {/* 
-      <button className="settings">
-        <UserSettings />
-      </button> */}
+      <img src={getUserPhotoUrl()} alt="" />
     </div>
   );
 };
--- src/components/left-bar/LeftBar.jsx
+++ src/components/left-bar/LeftBar.jsx
@@ -2,12 +2,10 @@ import "./style.css";
 import Header from "../header/Header";
 
 import ChatList from "../chat-list/ChatList";
-
 import NewChat from "../newChat/NewChat";
 import Preloader from "../preloader/preloader";
 
 const LeftBar = ({ chatListLoading, chatList }) => {
-  console.log(chatListLoading, chatList);
   return (
     <section className="leftbar">
       <Header />
